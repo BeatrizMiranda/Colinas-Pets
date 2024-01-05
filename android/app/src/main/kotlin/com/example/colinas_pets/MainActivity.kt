82d6234656601e50fe7563a9098f862dd4f86b70
Beatriz Miranda <ext.beatriz.silva@farfetch.com>
2024-01-05 09:37:34 -0300

    feat: initial commit

---
+++ android/app/src/main/kotlin/com/example/colinas_pets/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.colinas_pets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/animals_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnimalsList extends StatefulWidget {
+  const AnimalsList({super.key});
+
+  @override
+  State<AnimalsList> createState() => _AnimalsListState();
+}
+
+class _AnimalsListState extends State<AnimalsList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
